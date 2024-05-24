bfe94741309fdefe34931b19c381ed3d7fd8bfd2
J jet <ovepje2004@gmail.com>
2024-05-24 21:59:36 +0900

    gate

---
+++ Constants.h
@@ -0,0 +1,31 @@
+#ifndef CONSTANTS
+
+#define CONSTANTS
+
+// Map Configurations
+#define MAP_SIZE 20
+
+// Snake Configurations
+#define MIN_LENGTH 1
+#define MAX_LENGTH 30
+
+// Tick Configurations
+#define TICK 100
+
+// Elements
+#define UP 0
+#define DOWN 1
+#define RIGHT 2
+#define LEFT 3
+#define START_X  8
+#define START_Y  8
+#define BLANK 0
+#define WALL 1
+#define IMMUNE_WALL 2
+#define SNAKE_HEAD 3
+#define SNAKE_BODY 4
+#define GROWTH_ITEM 5
+#define POISON_ITEM 6
+#define GATE 7
+
+#endif
