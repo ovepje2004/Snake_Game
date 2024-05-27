0f5e74c6e1b116870966b81b6ef25ed7f1d7811a
J jet <ovepje2004@gmail.com>
2024-05-28 00:56:07 +0900

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -13,7 +13,6 @@
 
 using namespace std;
 
-
 void printb(const int row, const int column, const int color_pair) {
 	move(row, column * 2);
 	attron(COLOR_PAIR(color_pair));
@@ -24,7 +23,7 @@ void printb(const int row, const int column, const int color_pair) {
 
 // 맵 파일에서 맵 불러오기
 Map map;
-Gate getRandomGate() {
+Gate getRandomGate1() {
 	int x, y;
 	do {
 		x = rand() % MAP_SIZE;
@@ -32,6 +31,30 @@ Gate getRandomGate() {
 	} while (map.map[x][y] != 1);
 
 	return Gate(x, y);
+}
+
+Gate getRandomGate2(int avoidX, int avoidY) {
+	int x, y;
+	do {
+		x = rand() % MAP_SIZE;
+		y = rand() % MAP_SIZE;
+	} while ((x == avoidX && y == avoidY) || map.map[x][y] != 1);
+
+	return Gate(x, y);
+}
+
+void Item_respawn(Item& item, const Map map) {
+	// 위치가 겹치지 않게 아이템 생성
+	int new_x, new_y;
+	do {
+		new_x = std::rand() % MAP_SIZE;
+		new_y = std::rand() % MAP_SIZE;
+	} while (map.map[new_x][new_y] != 0);
+
+	item.x = new_x;
+	item.y = new_y;
+	item.Item_type = std::rand() % 2;
+	item.active = true;
 }
 
 int main() {
@@ -43,6 +66,8 @@ int main() {
 	init_pair(3, COLOR_WHITE, COLOR_WHITE);
 	init_pair(4, COLOR_YELLOW, COLOR_YELLOW);
 	init_pair(5, COLOR_GREEN, COLOR_GREEN);
+	init_pair(6, COLOR_BLACK, COLOR_BLACK);
+	init_pair(7, COLOR_CYAN, COLOR_CYAN);
 	init_pair(8, COLOR_MAGENTA, COLOR_MAGENTA);
 	// 필요하면 컬러페어 수정하거나 추가해주세요!
 
@@ -66,9 +91,10 @@ int main() {
 	itemTick = 40;
 	itemNum = 3;
 
+	halfdelay(3);
 	// 스테이지 클리어하면 stage 변수가 1씩 증가
 	// victory가 true인 상태로 반복문 탈출하면 게임 최종 승리, 아니면 실패
-	for (int stage = 1; ; ++stage) {
+	for (int stage = 5; ; ++stage) {
 		// 더 불러올 맵이 없으면 승리 처리 및 반복문 탈출
 		std::ifstream fin("map" + to_string(stage) + ".txt");
 		if (!fin) {
@@ -79,7 +105,6 @@ int main() {
 		// 최종적으로 화면에 출력할 내용을 배열로 구현
 		int screen[MAP_SIZE][MAP_SIZE];
 
-		
 		for (int row = 0; row < MAP_SIZE; ++row)
 			for (int column = 0; column < MAP_SIZE; ++column)
 				fin >> map.map[row][column];
@@ -87,24 +112,25 @@ int main() {
 		// 길이 3칸, 왼쪽, 맵 중앙으로 지렁이 초기화
 		Snake snake(3, LEFT, MAP_SIZE / 2, MAP_SIZE / 2);
 
+		//items 객체 생성 및 초기화 
 		Item items[3];
-		/*준서: items 초기화하기
-		* Item 객체 배열 items를 위치가 랜덤하게 초기화하세요.
-		* Item의 종류(GrowthItem, PoisonItem)는 무작위입니다.
-		*/
+		for (int i = 0; i < itemNum; i++) {
+			Item_respawn(items[i], map);
+		}
 
 		Gate gates[2];
 
 		srand(time(nullptr));
-		
-		for (int i = 0; i < 2; i++) {
-			Gate randomGate = getRandomGate();
-			int x = randomGate.getX();
-			int y = randomGate.getY();
-			gates[i] = Gate(x, y);
-		}
-		
 
+		Gate randomGate1 = getRandomGate1();
+		int x1 = randomGate1.getX();
+		int y1 = randomGate1.getY();
+		gates[0] = Gate(x1, y1);
+
+		Gate randomGate2 = getRandomGate2(x1, y1);
+		int x2 = randomGate2.getX();
+		int y2 = randomGate2.getY();
+		gates[1] = Gate(x2, y2);
 
 		gateActive = false;
 		victory = false;
@@ -117,168 +143,11 @@ int main() {
 				for (int column = 0; column < MAP_SIZE; ++column)
 					screen[row][column] = map.map[row][column];
 
-			/*
 			// screen에 item 올리기
-			// 준서: 아래 코드의 조건문을 완성해주세요.
 			for (int index = 0; index < itemNum; ++index) {
 				screen[items[index].x][items[index].y]
-					= (items[index]가 GrowthItem의 객체일 때 true) ? GROWTH_ITEM : POISON_ITEM;
+					= (items[index].Item_type == 1) ? GROWTH_ITEM : POISON_ITEM;
 			}
-			*/
 
 			// screen에 gate 올리기
 			// 정은: 아래 코드를 완성해주세요.
-			for (Gate gate : gates) {
-				// gate의 위치에 해당하는 위치를 screen에서 GATE(7)로 설정
-				screen[gate.x][gate.y] = 7;
-
-			}
-
-			// screen에 snake 올리기
-			for (int index = 0; index < snake.getLength(); ++index) {
-				screen[snake.getPositionOf(index)[0]][snake.getPositionOf(index)[1]]
-					= (index == 0) ? SNAKE_HEAD : SNAKE_BODY;
-			}
-
-			// screen을 화면에 출력
-			for (int row = 0; row < MAP_SIZE; ++row)
-				for (int column = 0; column < MAP_SIZE; ++column)
-					printb(START_X + row, START_Y + column, screen[row][column] + 1);
-			refresh();
-
-			
-
-			// 준서: 1틱(0.25초)동안 입력 받으며 대기하기
-			// 0.25초가 지나면 tick 변수를 1 증가시킨다.
-			// ClassName time = now();
-			//while (/* now() - time < 250ms */)
-			keyboardInput = getch();    // 이 방법은 문제가 있다!
-			++tick;
-
-			// 지렁이가 움직일 때
-			if (tick % snakeTick == 0) {
-				// 반대방향 입력하면 실패
-				if ((keyboardInput == KEY_UP && snake.getDirection() == DOWN) ||
-					(keyboardInput == KEY_DOWN && snake.getDirection() == UP) ||
-					(keyboardInput == KEY_RIGHT && snake.getDirection() == LEFT) ||
-					(keyboardInput == KEY_LEFT && snake.getDirection() == RIGHT))
-					break;
-
-				// 이동
-				switch (keyboardInput) {
-				case KEY_UP:    snake.setDirectionTo(UP);  break;
-				case KEY_DOWN:  snake.setDirectionTo(DOWN); break;
-				case KEY_RIGHT: snake.setDirectionTo(RIGHT); break;
-				case KEY_LEFT:  snake.setDirectionTo(LEFT);  break;
-				}
-				snake.move();
-
-				// 자기 몸체에 충돌하면 실패
-				for (int index = 1; index < snake.getLength(); ++index)
-					if (snake.getPositionOf(0) == snake.getPositionOf(index))
-						break;
-
-				// 벽에 부딪히면 실패
-				if ((map.map[snake.getPositionOf(0)[0]][snake.getPositionOf(0)[1]] == WALL ||
-					map.map[snake.getPositionOf(0)[0]][snake.getPositionOf(0)[1]] == IMMUNE_WALL) && 
-					!((snake.getPositionOf(0)[0] == gates[0].x && snake.getPositionOf(0)[1] == gates[0].y) || 
-					(snake.getPositionOf(0)[0] == gates[1].x && snake.getPositionOf(0)[1] == gates[1].y))) {
-					break;
-				}
-
-				/*
-				// 아이템 획득
-				for (int index = 0; index < itemNum; ++index) {
-					// items[index]와 위치가 겹치면
-					if (snake.getPositionOf(0)[0] == items[index].x &&
-						snake.getPositionOf(0)[1] == items[index].y) {
-						if (items[index]가 GrowthItem의 객체일 경우 true)
-							snake.increaseLength();
-						else
-							snake.decreaseLength();
-
-						준서: 아이템 리스폰
-						* 획득한 해당 아이템, 즉 items[index]를 재생성하세요.
-						* GrowthItem과 PoisonItem 중 하나로 무작위생성합니다.
-						* 위치도 다른 요소들과 겹치지 않도록 무작위로 설정합니다.
-						
-
-						break;
-					}
-				}
-				*/
-				// 목표 길이 도달하면 스테이지 클리어
-				if (snake.getLength() >= goal) {
-					victory = true;
-					break;
-				}
-
-				// 최소 길이보다 짧아지면 실패
-				if (snake.getLength() < MIN_LENGTH)
-					break;
-
-				for (int i = 0; i < 2; ++i) {
-					if (snake.getPositionOf(0)[0] == gates[i].x && snake.getPositionOf(0)[1] == gates[i].y) {
-						int oppositeGateIndex = (i == 0) ? 1 : 0;
-						int wall = gates[oppositeGateIndex].checkWall();
-						int XgoTo = 0; int YgoTo = 0;
-						switch (wall) {
-							case 1:
-								snake.setDirectionTo(RIGHT);
-								XgoTo = gates[oppositeGateIndex].x + 1;
-								YgoTo = gates[oppositeGateIndex].y;
-								break;
-							case RIGHT:
-								snake.setDirectionTo(LEFT);
-								XgoTo = gates[oppositeGateIndex].x - 1;
-								YgoTo = gates[oppositeGateIndex].y;
-								break;
-							case 3:
-								snake.setDirectionTo(DOWN);
-								XgoTo = gates[oppositeGateIndex].x;
-								YgoTo = gates[oppositeGateIndex].y + 1;
-								break;
-							case 4:
-								snake.setDirectionTo(UP);
-								XgoTo = gates[oppositeGateIndex].x;
-								YgoTo = gates[oppositeGateIndex].y - 1;
-								break;
-							default:
-								break;
-						}
-						snake.setPositionTo(XgoTo, YgoTo);
-						break;
-					}
-				}
-				
-				
-			}
-
-			/*
-			if (tick % itemTick == 0) {
-				준서: 아이템 재출현
-				* items에 있는 itemNum개의 아이템을 재출현시켜주세요.
-				* Item의 종류(GrowthItem, PoisonItem)는 무작위입니다.
-				
-			}
-			*/
-
-		}
-
-		// victory가 false인 상태로 반복문 탈출했으면
-		// 실패 처리하고 게임 종료
-		if (!victory)
-			break;
-
-		// 다음 스테이지로 넘어가기까지 잠깐 멈춤
-		Sleep(5000);
-	}
-
-	endwin();
-	if (victory)
-		cout << "contratulations!" << endl;
-	else
-		cout << "failed!" << endl;
-
-	return 0;
-}
