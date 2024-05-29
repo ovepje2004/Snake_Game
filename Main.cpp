6146ff6283b1e14faf029467c00c1c68e79aaf0f
J jet <ovepje2004@gmail.com>
2024-05-29 20:03:51 +0900

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -151,3 +151,147 @@ int main() {
 
 			// screen에 gate 올리기
 			// 정은: 아래 코드를 완성해주세요.
+			for (Gate gate : gates) {
+				// gate의 위치에 해당하는 위치를 screen에서 GATE(7)로 설정
+				screen[gate.x][gate.y] = 7;
+
+			}
+
+			// screen에 snake 올리기
+			for (int index = 0; index < snake.getLength(); ++index) {
+				screen[snake.getPositionOf(index)[0]][snake.getPositionOf(index)[1]]
+					= (index == 0) ? SNAKE_HEAD : SNAKE_BODY;
+			}
+
+			// screen을 화면에 출력
+			for (int row = 0; row < MAP_SIZE; ++row)
+				for (int column = 0; column < MAP_SIZE; ++column)
+					printb(START_X + row, START_Y + column, screen[row][column] + 1);
+			refresh();
+
+			keyboardInput = getch();
+			++tick;
+
+			// 지렁이가 움직일 때
+			if (tick % snakeTick == 0) {
+				// 반대방향 입력하면 실패
+				if ((keyboardInput == KEY_UP && snake.getDirection() == DOWN) ||
+					(keyboardInput == KEY_DOWN && snake.getDirection() == UP) ||
+					(keyboardInput == KEY_RIGHT && snake.getDirection() == LEFT) ||
+					(keyboardInput == KEY_LEFT && snake.getDirection() == RIGHT))
+					break;
+
+				// 이동
+				switch (keyboardInput) {
+				case KEY_UP:    snake.setDirectionTo(UP);  break;
+				case KEY_DOWN:  snake.setDirectionTo(DOWN); break;
+				case KEY_RIGHT: snake.setDirectionTo(RIGHT); break;
+				case KEY_LEFT:  snake.setDirectionTo(LEFT);  break;
+				}
+				snake.move();
+
+				// 자기 몸체에 충돌하면 실패
+				bool crashed = false;
+				for (int index = 1; index < snake.getLength(); ++index)
+					if (snake.getPositionOf(0)[0] == snake.getPositionOf(index)[0] &&
+						snake.getPositionOf(0)[1] == snake.getPositionOf(index)[1]) {
+						crashed = true;
+						break;
+					}
+				if (crashed) break;
+
+				// 벽에 부딪히면 실패
+				if ((map.map[snake.getPositionOf(0)[0]][snake.getPositionOf(0)[1]] == WALL ||
+					map.map[snake.getPositionOf(0)[0]][snake.getPositionOf(0)[1]] == IMMUNE_WALL) &&
+					!((snake.getPositionOf(0)[0] == gates[0].x && snake.getPositionOf(0)[1] == gates[0].y) ||
+					(snake.getPositionOf(0)[0] == gates[1].x && snake.getPositionOf(0)[1] == gates[1].y))) {
+					break;
+				}
+
+				// 아이템 획득
+				for (int index = 0; index < itemNum; ++index) {
+					// items[index]와 위치가 겹치면
+					if (snake.getPositionOf(0)[0] == items[index].x &&
+						snake.getPositionOf(0)[1] == items[index].y) {
+						if (items[index].Item_type == 1) //growthItem일 경우 길이 증가
+							snake.increaseLength();
+						else //poisonItem일 경우 길이 감소
+							snake.decreaseLength();
+
+
+						Item_respawn(items[index], map);
+						//아이템이 먹힌 경우 리스폰 뿐만 아니라, 아이템을 화면에서 잠시 숨겨야 함.
+						items[index].active = false;
+
+						break;
+					}
+				}
+
+				// 목표 길이 도달하면 스테이지 클리어
+				if (snake.getLength() >= goal) {
+					victory = true;
+					break;
+				}
+
+				// 최소 길이보다 짧아지면 실패
+				if (snake.getLength() < MIN_LENGTH)
+					break;
+
+				for (int i = 0; i < 2; ++i) {
+					if (snake.getPositionOf(0)[0] == gates[i].x && snake.getPositionOf(0)[1] == gates[i].y) {
+						int oppositeGateIndex = (i == 0) ? 1 : 0;
+						int wall = gates[oppositeGateIndex].checkWall();
+						int XgoTo = 0; int YgoTo = 0;
+						switch (wall) {
+						case 1:
+							snake.setDirectionTo(DOWN);
+							XgoTo = gates[oppositeGateIndex].x + 1;
+							YgoTo = gates[oppositeGateIndex].y;
+							break;
+						case RIGHT:
+							snake.setDirectionTo(UP);
+							XgoTo = gates[oppositeGateIndex].x - 1;
+							YgoTo = gates[oppositeGateIndex].y;
+							break;
+						case 3:
+							snake.setDirectionTo(RIGHT);
+							XgoTo = gates[oppositeGateIndex].x;
+							YgoTo = gates[oppositeGateIndex].y + 1;
+							break;
+						case 4:
+							snake.setDirectionTo(LEFT);
+							XgoTo = gates[oppositeGateIndex].x;
+							YgoTo = gates[oppositeGateIndex].y - 1;
+							break;
+						default:
+							break;
+						}
+						snake.setPositionTo(XgoTo, YgoTo);
+						break;
+					}
+				}
+			}
+
+			if (tick % itemTick == 0) {
+				//아이템 재출현
+				for (int i = 0; i < itemNum; i++)  Item_respawn(items[i], map);
+			}
+		}
+
+		// victory가 false인 상태로 반복문 탈출했으면
+		// 실패 처리하고 게임 종료
+		if (!victory)
+			break;
+
+		// 다음 스테이지로 넘어가기까지 잠깐 멈춤
+		Sleep(5000);
+	}
+
+	endwin();
+	if (victory)
+		cout << "contratulations!" << endl;
+	else
+		cout << "failed!" << endl;
+
+	return 0;
+}
